3aaf9b0722782a3c0f860ec92c63549a64c42ddf
crowtek <prt3@hotmail.de>
2022-02-26 19:15:18 +0100

    navbar update

--- Navbar/app.js
+++ Navbar/app.js
@@ -3,3 +3,11 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+const toggleBtn = document.querySelector(".nav-toggle");
+const links = document.querySelector(".link");
+
+toggleBtn.addEventListener("click",  () => {
+    links.classList.add(".show-links")
+});
+
